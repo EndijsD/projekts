e45f6b776a516f0d1d12f750da37c84781e000fc
Endijs Dārznieks <endijsdarznieks@gmail.com>
2023-04-26 00:42:44 +0300

    Atjaunināju servera puses saiti

--- sistema/client/src/url.js
+++ sistema/client/src/url.js
@@ -1,5 +1,5 @@
 const protocol = 'https';
-const ip = 'temp-server.up.railway.app';
+const ip = 'sistema-backend.up.railway.app';
 const port = '3001';
 
 const url = protocol + '://' + ip + '/'; // ':' + port + '/';
