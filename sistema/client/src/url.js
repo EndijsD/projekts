17781781a8c7f12dbc153d3f0a2d4a4dd4528001
Endijs Dārznieks <endijsdarznieks@gmail.com>
2023-04-25 23:05:33 +0300

    Pārmainīju klienta puses servera url

--- sistema/client/src/url.js
+++ sistema/client/src/url.js
@@ -1,6 +1,7 @@
-const protocol = 'http';
-const ip = 'localhost';
+const protocol = 'https';
+const ip = 'temp-server.up.railway.app';
 const port = '3001';
 
-const url = protocol + '://' + ip + ':' + port + '/';
+const url = protocol + '://' + ip + '/'; // ':' + port + '/';
+
 export default url;
